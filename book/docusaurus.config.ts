ff44fc6370b29f8d3e2944bd0fb5c81ac733dd57
1026316503 <1026316503@qq.com>
2025-07-01 17:48:45 +0800

    adad

--- book/docusaurus.config.ts
+++ book/docusaurus.config.ts
@@ -1,6 +1,8 @@
 import {themes as prismThemes} from 'prism-react-renderer';
 import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 const config: Config = {
   title: '智能的觉醒', // 替换为你的书籍标题
@@ -44,12 +46,23 @@ const config: Config = {
           // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/whirling-ai-consortium/The-Awakening-of-Intelligence-A-Chronicle-of-Large-Language-Models/edit/main/', // 修改为你的仓库编辑链接
+            remarkPlugins: [remarkMath],
+          rehypePlugins: [rehypeKatex],
         },
         theme: {
           customCss: './src/css/custom.css',
         },
       } satisfies Preset.Options,
     ],
+  ],
+  stylesheets: [
+    {
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+      type: 'text/css',
+      integrity:
+        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+      crossorigin: 'anonymous',
+    },
   ],
 
   themeConfig: {
@@ -102,6 +115,8 @@ const config: Config = {
             {
               label: 'GitHub',
               href: 'https://github.com/whirling-ai-consortium/The-Awakening-of-Intelligence-A-Chronicle-of-Large-Language-Models', // 修改为你的 GitHub 仓库链接
+              remarkPlugins: [remarkMath],
+          rehypePlugins: [rehypeKatex],
             },
           ],
         },
